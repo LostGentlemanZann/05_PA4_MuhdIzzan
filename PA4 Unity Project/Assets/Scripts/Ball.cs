f7ad054765e4759c34ed5d6627b026402b9ad77e
LostGentlemanZann <RubyPhoenix01@hotmail.com>
2020-05-04 17:18:39 +0800

    Unity Project Added
    Unity Project Added

---
+++ PA4 Unity Project/Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public int speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 5, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            transform.position -= new Vector3(0, 0, 1) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, 0, 1) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(0, 200, 0);
+        }
+    }
+}
